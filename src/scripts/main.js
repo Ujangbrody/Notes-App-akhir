f6daa45831adadf8f96b63eb82400e21b637c0cb
Hassan <hssnfalih@gmail.com>
2024-04-05 00:12:35 +0700

    add feature archive & non archive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,26 @@
+// Variabel untuk menyimpan id catatan yang dipilih
 let selectedNoteId;
 
+// Definisi kelas NoteElement
 class NoteElement extends HTMLElement {
     constructor() {
         super();
-        this.addEventListener('click', () => this.openEditPopup());
+        this.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.classList.contains('archive-btn')) {
+                this.archiveNote();
+            } else if (target.classList.contains('unarchive-btn')) {
+                this.unarchiveNote();
+            }
+        });
     }
 
+    // Metode untuk membuka popup edit
     openEditPopup() {
-        // Definisikan logika untuk membuka popup edit di sini
         console.log('Edit popup opened');
     }
 
-    
+    // Metode yang dipanggil ketika elemen terhubung
     connectedCallback() {
         const { id, title, body } = this.dataset;
         this.innerHTML = `
@@ -19,37 +28,61 @@ class NoteElement extends HTMLElement {
             <h2>${title}</h2>
             <p>${body}</p>
             <div class="note-actions">
-              <button onclick="updateNote('${id}')"><i class="fa-solid fa-user-pen"></i>update</button>
-              <button onclick="removeNote('${id}')"><i class="fa-solid fa-trash"></i>delete</button>
+              <button onclick="removeNote('${id}')">delete</button>
+              <button class="archive-btn">Archive Note</button>
+              <button class="unarchive-btn">Unarchive Note</button>
             </div>
           </div>
         `;
     }
+
+    // Metode untuk mengarsipkan catatan
+    async archiveNote() {
+        const id = this.dataset.id;
+        try {
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Auth-Token': '12345',
+                },
+                body: JSON.stringify({ archived: true }),
+            };
+
+            const response = await fetch(`${baseUrl}/notes/${id}/archive`, options);
+            const responseJson = await response.json();
+            showResponseMessage(responseJson.message);
+            getNote();
+        } catch (error) {
+            showResponseMessage(error);
+        }
+    }
+
+    // Metode untuk membatalkan pengarsipan catatan
+    async unarchiveNote() {
+        const id = this.dataset.id;
+        try {
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Auth-Token': '12345',
+                },
+                body: JSON.stringify({ archived: false }),
+            };
+
+            const response = await fetch(`${baseUrl}/notes/${id}/unarchive`, options);
+            const responseJson = await response.json();
+            showResponseMessage(responseJson.message);
+            getNote();
+        } catch (error) {
+            showResponseMessage(error);
+        }
+    }
 }
 customElements.define("note-element", NoteElement);
 
-window.removeNote = async (id) => {
-  try {
-      const confirmDelete = confirm('Are you sure you want to delete this note?');
-      if (confirmDelete) {
-          const options = {
-              method: 'DELETE',
-              headers: {
-                  'Content-Type': 'application/json',
-                  'X-Auth-Token': '12345',
-              },
-          };
-
-          const response = await fetch(`${baseUrl}/notes/${id}`, options);
-          const responseJson = await response.json();
-          showResponseMessage(responseJson.message);
-          getNote(); // Ambil data catatan setelah berhasil menghapus catatan
-      }
-  } catch (error) {
-      showResponseMessage(error);
-  }
-};
-
+// Definisi kelas AppBar
 class AppBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -57,16 +90,12 @@ class AppBar extends HTMLElement {
             border-bottom-right-radius: 10px;">
                 <h1 style="margin: 0;">My Notes</h1>
             </header>
-            
         `;
     }
 }
 customElements.define("app-bar", AppBar);
 
-window.addEventListener('DOMContentLoaded', () => {
-    getNote(); // Panggil fungsi getNote() saat halaman dimuat
-});
-
+// Definisi kelas NoteInput
 class NoteInput extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -74,37 +103,36 @@ class NoteInput extends HTMLElement {
                 <textarea id="noteSubject" name="story" rows="2" cols="33" placeholder="Judul"></textarea>
                 <textarea id="noteContent" name="story" rows="10" cols="50" placeholder="Isi Note"></textarea>
                 <button id="submitNote">Submit</button>
-                <button onclick="getNonArchivedNotes()">Get Non-Archived Notes</button>
-              <button onclick="getArchivedNotes()">Get Archived Notes</button>
-              <button onclick="getSingleNote()">Get Single Note</button>
-              <button onclick="archiveNote()">Archive Note</button>
-              <button onclick="unarchiveNote()">Unarchive Note</button>
+                <select id="archiveOptions">
+                    <option value="archive">Archive</option>
+                    <option value="non-archive">Non Archive</option>
+                </select>
             </div>
-            
         `;
 
         const submitButton = this.querySelector('#submitNote');
         submitButton.addEventListener('click', () => {
             const noteSubjectInput = this.querySelector('#noteSubject');
             const noteContentInput = this.querySelector('#noteContent');
-
-            insertNote(noteSubjectInput.value, noteContentInput.value); // Panggil fungsi insertNote()
+            insertNote(noteSubjectInput.value, noteContentInput.value);
         });
     }
 }
 customElements.define("note-input", NoteInput);
 
+// Fungsi untuk menampilkan pesan respons
 const showResponseMessage = (message = 'Check your internet connection') => {
     alert(message);
 };
 
+// Base URL untuk API catatan
 const baseUrl = 'https://notes-api.dicoding.dev/v2';
 
+// Fungsi untuk mendapatkan catatan dari server
 const getNote = async () => {
     try {
         const response = await fetch(`${baseUrl}/notes`);
         const responseJson = await response.json();
-        console.log(responseJson.data);
         if (responseJson.error) {
             showResponseMessage(responseJson.message);
         } else {
@@ -115,6 +143,41 @@ const getNote = async () => {
     }
 };
 
+// Fungsi untuk mendapatkan catatan non-archived dari server
+const getNoteNonArchive = async () => {
+    try {
+        const response = await fetch(`${baseUrl}/notes`);
+        const responseJson = await response.json();
+        if (responseJson.error) {
+            showResponseMessage(responseJson.message);
+        } else {
+            // Filter catatan yang belum diarsipkan
+            const nonArchivedNotes = responseJson.data.filter(note => !note.archived);
+            renderNonArchivedNotes(nonArchivedNotes);
+        }
+    } catch (error) {
+        showResponseMessage(error);
+    }
+};
+
+// Fungsi untuk mendapatkan catatan archived dari server
+const getNoteArchive = async () => {
+    try {
+        const response = await fetch(`${baseUrl}/notes/archived`);
+        const responseJson = await response.json();
+        if (responseJson.error) {
+            showResponseMessage(responseJson.message);
+        } else {
+            // Filter catatan yang sudah diarsipkan
+            const archivedNotes = responseJson.data.filter(note => note.archived);
+            renderArchivedNotes(archivedNotes);
+        }
+    } catch (error) {
+        showResponseMessage(error);
+    }
+};
+
+// Fungsi untuk menambahkan catatan baru
 const insertNote = async (title, body) => {
     try {
         const note = {
@@ -134,12 +197,13 @@ const insertNote = async (title, body) => {
         const response = await fetch(`${baseUrl}/notes`, options);
         const responseJson = await response.json();
         showResponseMessage(responseJson.message);
-        getNote(); // Ambil data catatan baru setelah berhasil membuat catatan
+        getNote();
     } catch (error) {
         showResponseMessage(error);
     }
 };
 
+// Fungsi untuk menghapus catatan
 const removeNote = async (id) => {
     try {
         const confirmRemove = confirm('Are you sure you want to remove this note?');
@@ -155,13 +219,25 @@ const removeNote = async (id) => {
             const response = await fetch(`${baseUrl}/notes/${id}`, options);
             const responseJson = await response.json();
             showResponseMessage(responseJson.message);
-            getNote(); // Ambil data catatan baru setelah berhasil menghapus catatan
+            getNote();
         }
     } catch (error) {
         showResponseMessage(error);
     }
 };
 
+// Event listener untuk opsi archiveOptions
+const archiveOptions = document.getElementById('archiveOptions');
+archiveOptions.addEventListener('change', () => {
+    const selectedOption = archiveOptions.value;
+    if (selectedOption === 'archive') {
+        getNoteArchive();
+    } else if (selectedOption === 'non-archive') {
+        getNoteNonArchive();
+    }
+});
+
+// Fungsi untuk merender catatan
 const renderNotes = (notes) => {
     const notesContainer = document.getElementById('notesContainer');
     notesContainer.innerHTML = '';
@@ -175,142 +251,41 @@ const renderNotes = (notes) => {
     });
 };
 
-// Membuat catatan baru
-const createNote = async (title, body) => {
-    try {
-        const note = {
-            title: title,
-            body: body
-        };
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Auth-Token': '12345',
-            },
-            body: JSON.stringify(note),
-        };
+// Fungsi untuk merender catatan yang diarsipkan
+const renderArchivedNotes = (notes) => {
+    const notesContainer = document.getElementById('notesContainer');
+    notesContainer.innerHTML = '';
 
-        const response = await fetch(`${baseUrl}/notes`, options);
-        const responseJson = await response.json();
-        showResponseMessage(responseJson.message);
-        getNote(); // Ambil data catatan baru setelah berhasil membuat catatan
-    } catch (error) {
-        showResponseMessage(error);
-    }
-};
-
-// Mendapatkan catatan non-archived
-const getNonArchivedNotes = async () => {
-    try {
-        const response = await fetch(`${baseUrl}/notes?archived=false`);
-        const responseJson = await response.json();
-        console.log(responseJson.data);
-        if (responseJson.error) {
-            showResponseMessage(responseJson.message);
-        } else {
-            renderNotes(responseJson.data);
+    notes.forEach((note) => {
+        if (note.archived) {
+            const noteElement = document.createElement('note-element');
+            noteElement.dataset.id = note.id;
+            noteElement.dataset.title = note.title;
+            noteElement.dataset.body = note.body;
+            notesContainer.appendChild(noteElement);
         }
-    } catch (error) {
-        showResponseMessage(error);
-    }
+    });
 };
 
-// Mendapatkan catatan yang diarsipkan
-const getArchivedNotes = async () => {
-    try {
-        const response = await fetch(`${baseUrl}/notes?archived=true`);
-        const responseJson = await response.json();
-        console.log(responseJson.data);
-        if (responseJson.error) {
-            showResponseMessage(responseJson.message);
-        } else {
-            renderNotes(responseJson.data);
+// Fungsi untuk merender catatan yang belum diarsipkan
+const renderNonArchivedNotes = (notes) => {
+    const notesContainer = document.getElementById('notesContainer');
+    notesContainer.innerHTML = '';
+
+    notes.forEach((note) => {
+        if (!note.archived) {
+            const noteElement = document.createElement('note-element');
+            noteElement.dataset.id = note.id;
+            noteElement.dataset.title = note.title;
+            noteElement.dataset.body = note.body;
+            notesContainer.appendChild(noteElement);
         }
-    } catch (error) {
-        showResponseMessage(error);
-    }
+    });
 };
 
-// Mendapatkan catatan berdasarkan ID
-const getSingleNote = async (id) => {
-    try {
-        const response = await fetch(`${baseUrl}/notes/${id}`);
-        const responseJson = await response.json();
-        console.log(responseJson.data);
-        if (responseJson.error) {
-            showResponseMessage(responseJson.message);
-        } else {
-            // Kembalikan data catatan
-            return responseJson.data;
-        }
-    } catch (error) {
-        showResponseMessage(error);
-    }
-};
 
-// Mengarsipkan catatan
-const archiveNote = async (id) => {
-    try {
-        const options = {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Auth-Token': '12345',
-            },
-            body: JSON.stringify({ archived: true }),
-        };
-
-        const response = await fetch(`${baseUrl}/notes/${id}`, options);
-        const responseJson = await response.json();
-        showResponseMessage(responseJson.message);
-        getNote(); // Ambil data catatan setelah berhasil mengarsipkan catatan
-    } catch (error) {
-        showResponseMessage(error);
-    }
-};
-
-// Membatalkan pengarsipan catatan
-const unarchiveNote = async (id) => {
-    try {
-        const options = {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Auth-Token': '12345',
-            },
-            body: JSON.stringify({ archived: false }),
-        };
-
-        const response = await fetch(`${baseUrl}/notes/${id}`, options);
-        const responseJson = await response.json();
-        showResponseMessage(responseJson.message);
-        getNote(); // Ambil data catatan setelah berhasil membatalkan pengarsipan catatan
-    } catch (error) {
-        showResponseMessage(error);
-    }
-};
-
-// Menghapus catatan
-window.deleteNote = async (id) => {
-    try {
-        const confirmDelete = confirm('Are you sure you want to delete this note?');
-        if (confirmDelete) {
-            const options = {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Auth-Token': '12345',
-                },
-            };
-
-            const response = await fetch(`${baseUrl}/notes/${id}`, options);
-            const responseJson = await response.json();
-            showResponseMessage(responseJson.message);
-            getNote(); // Ambil data catatan setelah berhasil menghapus catatan
-        }
-    } catch (error) {
-        showResponseMessage(error);
-    }
-};
+// Panggil fungsi getNoteNonArchive setelah halaman dimuat
+window.addEventListener('DOMContentLoaded', () => {
+    getNoteNonArchive();
+});
